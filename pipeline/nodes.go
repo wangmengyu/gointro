93b117b1835d228ce70185b228726145928ceac8
wangmengyu <mengyu.wang.0124@gmail.com>
2019-12-04 08:37:58 +0800

    merge data

--- pipeline/nodes.go
+++ pipeline/nodes.go
@@ -3,43 +3,45 @@ package pipeline
 import "sort"
 
 /**
-  建立 数据源收集器
+  get data source channel
 */
 func ArraySource(a ...int) <-chan int {
-	//建立一个接收器
+	//make a new out channel
 	out := make(chan int)
 	go func() {
 		for _, v := range a {
 			out <- v
 		}
-		//在协程处理完后，关闭管道
 		close(out)
 	}()
 	return out
 }
 
+/**
+  get data from channel,
+  sort data in memory,
+  put into out channel
+*/
 func InMemSort(in <-chan int) <-chan int {
-	//新建一个输出管道
+	//create new out channel
 	out := make(chan int)
-	//开协程对数据进行处理
+	//begin a goroutine to sort data
 	go func() {
-		//从管道读取数据到内存
+		//get data from channel,put into memory
 		a := make([]int, 0)
 		for i := range in {
 			a = append(a, i)
 		}
-		//排序
+		//sort
 		sort.Ints(a)
 
-		//输出给输出管道
+		//range data from memory and put into out channel
 		for _, v := range a {
 			out <- v
 		}
 		close(out)
 
 	}()
-
-	//返回管道
 	return out
 
 }
