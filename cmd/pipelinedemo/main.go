e75f4c72d123f1a637706b43ae35ebca14f7c856
wangmengyu <mengyu.wang.0124@gmail.com>
2019-12-04 08:34:00 +0800

    merge data

--- cmd/pipelinedemo/main.go
+++ cmd/pipelinedemo/main.go
@@ -7,10 +7,12 @@ import (
 
 func main() {
 	//建立数据源管道
-	p := pipeline.InMemSort(pipeline.ArraySource(3, 2, 6, 7, 4))
+	p := pipeline.InMemSort(pipeline.ArraySource(1, 3, 5, 7, 9))
+	p2 := pipeline.InMemSort(pipeline.ArraySource(2, 4, 6, 8, 0))
+	p3 := pipeline.Merge(p, p2)
 
 	//这里会 等待 管道有数据了就开始循环取出数据做处理,此处range 内部自己实现了等待机制
-	for i := range p {
+	for i := range p3 {
 		fmt.Println(i)
 	}
 
--- pipeline/nodes.go
+++ pipeline/nodes.go
@@ -43,3 +43,50 @@ func InMemSort(in <-chan int) <-chan int {
 	return out
 
 }
+
+/**
+  merge data from two channel, push data to out channel
+*/
+func Merge(in1, in2 <-chan int) <-chan int {
+	out := make(chan int)
+	//start a goroutine
+	go func() {
+
+		v1, ok1 := <-in1
+		v2, ok2 := <-in2
+		for {
+			if !ok1 && !ok2 {
+				//no one has value
+				break
+			}
+
+			if !ok2 {
+				//only ok1 has value
+				out <- v1
+				v1, ok1 = <-in1
+
+			} else if !ok1 {
+				//only ok2 has value
+				out <- v2
+				v2, ok2 = <-in2
+
+			} else {
+				//both has value
+				if v1 < v2 {
+					out <- v1
+					v1, ok1 = <-in1
+					out <- v2
+					v2, ok2 = <-in2
+				} else {
+					out <- v2
+					v2, ok2 = <-in2
+					out <- v1
+					v1, ok1 = <-in1
+				}
+			}
+		}
+		close(out)
+	}()
+
+	return out
+}
